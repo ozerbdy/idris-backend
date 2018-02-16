093b763eea06ae936afd40bfc900e7286aabf7fe
Özer Baday <ozer.baday@gmail.com>
2018-02-17 00:42:57 +0300

    Add response info constants

--- constants/constants.js
+++ constants/constants.js
@@ -1,3 +1,19 @@
+module.exports.ErrorInfo = {
+    RequestBodyValidationFailed: {
+        message: 'Request body validation failed',
+        code: 1
+    },
+    MongoError: {
+        message: 'Mongo operation failed',
+        code: 2
+    }
+};
+
+module.exports.SuccessInfo = {
+    message: 'Success',
+    code: 0
+};
+
 module.exports.CollectionNames = {
     users: 'users',
     packages: 'packages',
