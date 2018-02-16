ce094095f2a66959ed8f1838590c03ef3e5b3e85
Özer Baday <ozer.baday@gmail.com>
2018-02-17 02:19:07 +0300

    Add user getByToken query

--- db/UserRepository.js
+++ db/UserRepository.js
@@ -1,4 +1,6 @@
 const mongo = require('./mongo'),
+    ObjectId = require('mongodb').ObjectId,
+    is = require('is_js'),
     Constants = require('../constants/constants');
 
 const CollectionName = Constants.CollectionNames.users;
@@ -7,6 +9,15 @@ module.exports.getToken = function(username, password){
     const query = {
         username: username,
         password: password
+    };
+
+    return mongo.client.collection(CollectionName)
+        .findOne(query);
+};
+
+module.exports.get = function(token){
+    const query = {
+        _id: ObjectId(token)
     };
 
     return mongo.client.collection(CollectionName)
