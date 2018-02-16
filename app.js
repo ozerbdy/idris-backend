25a72fb60031943bbe9b2a108c7d1e1dbd3578a5
Özer Baday <ozer.baday@gmail.com>
2018-02-16 23:18:29 +0300

    Require mongodb credentials as environment variables

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express'),
     app = express(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
 
 const port = process.env.PORT || 3000;
+const mongoURL = process.env.MONGO_URL;
+const mongoDbName = process.env.MONGO_DB_NAME;
 
+if(!mongoURL || !mongoDbName) throw new Error('Mongodb credentials are not provided as environment variables!');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
