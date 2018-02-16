d5844932ba4d8ef3462e89cc30aa7652ba6e0d23
Özer Baday <ozer.baday@gmail.com>
2018-02-17 00:11:38 +0300

    Add user login endpoint to routes

--- routes.js
+++ routes.js
@@ -1,9 +1,12 @@
 const express = require('express'),
     Router = express.Router();
 
+const UserController = require('./controllers/user');
 
 Router.get('/', (req, res) => {
     return res.send('Hello! welcome to randHack webservice');
 });
+
+Router.post('/user/login', UserController.login);
 
 module.exports = Router;
