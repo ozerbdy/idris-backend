7521fb28728e9f84bb9b13f20842179565dcc616
Özer Baday <ozer.baday@gmail.com>
2018-02-17 02:24:14 +0300

    Add response helper function

---
+++ helpers/responseHelpers.js
@@ -0,0 +1,14 @@
+const Constants = require('../constants/constants');
+
+module.exports.getBasicResponseObject = function(ResponseType, extraMessage){
+
+    const ResponseCode = ResponseType.code;
+    const ResponseSuccess = ResponseCode === Constants.SuccessInfo.code;
+    let response = {
+        code: ResponseCode,
+        success: ResponseSuccess,
+        message: ResponseType.message
+    };
+    if (extraMessage) response.message += '; ' + extraMessage;
+    return response;
+};
