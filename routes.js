d684d456970555345e8c0aa2d7a771fe32511d90
Özer Baday <ozer.baday@gmail.com>
2018-02-17 00:11:16 +0300

    Add basic check endpoint

--- routes.js
+++ routes.js
@@ -1,4 +1,9 @@
 const express = require('express'),
     Router = express.Router();
 
+
+Router.get('/', (req, res) => {
+    return res.send('Hello! welcome to randHack webservice');
+});
+
 module.exports = Router;
