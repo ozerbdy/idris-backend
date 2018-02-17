8f4a1dc7d372613b3df7f4eb55277e02e0e7adf4
Özer Baday <ozer.baday@gmail.com>
2018-02-17 18:44:21 +0300

    Handle authentication error

--- middlewares/authenticator.js
+++ middlewares/authenticator.js
@@ -1,10 +1,18 @@
-const UserRepository = require('../db/UserRepository'),
-    Constants = require('../constants/constants');
+const is = require('is_js');
+const ResponseHelpers = require('../helpers/responseHelpers');
+const UserRepository = require('../db/UserRepository');
+const Constants = require('../constants/constants');
 
 
 module.exports.authenticate = async (req, res, next) => {
     const token = req.get(Constants.TOKEN_REQUEST_HEADER_NAME);
     const user = await UserRepository.get(token);
-    req.user = user;
-    next();
+    if(is.existy(user)){
+        req.user = user;
+        next();
+    }else{
+        return res.send({
+            status: ResponseHelpers.getBasicResponseObject(Constants.ErrorInfo.AuthenticationFailed)
+        });
+    }
 };
